650f9c328a1c6c4dba612277920198afea4b6b14
Esteban <esteban.horacio.acevedo@gmail.com>
2026-09-16 20:15:20 -0300

    views: prueba 2

--- src/config/urls.py
+++ src/config/urls.py
@@ -33,4 +33,5 @@ urlpatterns = [
 
 urlpatterns += [
     path("saludar/", views.saludar),
+    path("saludar2/", views.saludar2),
 ]
--- src/prueba/views.py
+++ src/prueba/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def saludar(request):
     return HttpResponse("Hola desde Django")
+
+
+def saludar2(request):
+    mensaje = "Mi App Web"
+    print(mensaje)
+    return HttpResponse(f"<p>Esta es una prueba de <b>{mensaje}</b></p>")
