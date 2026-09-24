cbd6c2aabb3223241acbc5e3f17edc7f24cf0c7d
esthorace <esteban.horacio.acevedo@gmail.com>
2026-09-23 22:05:31 -0300

    feat: crea modelo Pais y fk en Cliente

--- src/prueba/models.py
+++ src/prueba/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Pais(models.Model):
+    nombre = models.CharField()
 
 
 class Cliente(models.Model):
     nombre = models.CharField()
     apellido = models.CharField()
+    pais_origen = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self) -> str:
         return f"{self.apellido.upper()}, {self.nombre.capitalize()}"
