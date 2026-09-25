5b678358f47eb6024237f5b0bf4b0cbe2699d250
esthorace <esteban.horacio.acevedo@gmail.com>
2026-09-25 20:19:22 -0300

    feat: mejora modelo Categoria para forms

--- src/ventas/models.py
+++ src/ventas/models.py
@@ -1,13 +1,13 @@
-from typing import Any
-
 from django.db import models
 
 
 class Categoria(models.Model):
-    """Categoría de productos"""
-
     nombre = models.CharField(unique=True)
     descripcion = models.TextField(blank=True)
 
     def __str__(self) -> str:
         return self.nombre
+
+    class Meta:
+        verbose_name = "Categoría de Producto"
+        verbose_name_plural = "Categorías de Productos"
