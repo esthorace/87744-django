3e57a392033a3579ce95a5b5a878c6e2dc718b44
esthorace <esteban.horacio.acevedo@gmail.com>
2026-09-25 19:24:03 -0300

    refactor: anotaciones de tipo

--- src/prueba/views.py
+++ src/prueba/views.py
@@ -1,26 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 from prueba.models import Cliente
 
 
-def saludar(request):
+def saludar(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hola desde Django")
 
 
-def saludar_2(request):
+def saludar_2(request: HttpRequest) -> HttpResponse:
     mensaje = "Mi App Web"
     print(mensaje)
     return HttpResponse(f"<p>Esta es una prueba de <b>{mensaje}</b></p>")
 
 
-def parametros(request, nombre: str, apellido: str):
+def parametros(request: HttpRequest, nombre: str, apellido: str):
     nombre = nombre.capitalize()
     apellido = apellido.upper()
     return HttpResponse(f"<p><b>{apellido}</b>, {nombre}</p>")
 
 
-def home(request):
+def home(request: HttpRequest) -> HttpResponse:
     from datetime import UTC, datetime
 
     contexto = {
@@ -30,11 +30,11 @@ def home(request):
     return render(request, "prueba/home.html", contexto)
 
 
-def notas(request):
+def notas(request: HttpRequest) -> HttpResponse:
     lista_notas = [1, 10, 4, 7, 8, 5, 7]
     return render(request, "prueba/notas.html", {"notas": lista_notas})
 
 
-def cliente_list(request):
+def cliente_list(request: HttpRequest) -> HttpResponse:
     clientes = Cliente.objects.all()
     return render(request, "prueba/cliente_list.html", context={"clientes": clientes})
